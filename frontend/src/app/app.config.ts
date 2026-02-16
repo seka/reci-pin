9dcfa05cb4f4c241f56f3a180eac25d1d482a293
seka <s.seka134704@gmail.com>
2026-02-17 02:36:34 +0900

    feat(frontend): setup i18n with SSR support using TranslateServerLoader and I18N_ASSET_PATH

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,50 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, inject, PLATFORM_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withFetch, HttpClient } from '@angular/common/http';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Observable, of } from 'rxjs';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { TranslateServerLoader } from './core/i18n/translate-server.loader';
+
+
+
+class CustomHttpLoader implements TranslateLoader {
+  constructor(
+    private http: HttpClient,
+    private prefix: string = '/assets/i18n/',
+    private suffix: string = '.json'
+  ) { }
+
+  public getTranslation(lang: string): Observable<any> {
+    return this.http.get(`${this.prefix}${lang}${this.suffix}`);
+  }
+}
+
+export function httpLoaderFactory(http: HttpClient) {
+  const platformId = inject(PLATFORM_ID);
+  if (isPlatformBrowser(platformId)) {
+    return new CustomHttpLoader(http);
+  }
+  return new TranslateServerLoader();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])), provideClientHydration(withEventReplay()),
+    provideHttpClient(withInterceptors([authInterceptor]), withFetch()),
+    provideClientHydration(withEventReplay()),
+    provideTranslateService({
+      defaultLanguage: 'ja',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
 };
