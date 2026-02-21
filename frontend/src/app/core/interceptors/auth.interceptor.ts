7bb5d754d71c5c23a201f0640f421bbb804963f7
seka <s.seka134704@gmail.com>
2026-02-21 23:53:13 +0900

    feat(auth/frontend): remove redundant authInterceptor after migrating to cookie-based auth

--- frontend/src/app/core/interceptors/auth.interceptor.ts
+++
@@ -1,7 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  // ブラウザが Cookie (auth_token) を自動的に送信するため、
-  // 手動での Authorization ヘッダー付与は不要になりました。
-  return next(req);
-};
