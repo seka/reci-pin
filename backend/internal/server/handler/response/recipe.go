a163bba2c83031c1198cd0e8c8c08b794f04f327
seka <s.seka134704@gmail.com>
2026-02-23 14:15:18 +0900

    refactor(presentation): update handlers and responses for PublicRecipeImage

--- backend/internal/server/handler/response/recipe.go
+++ backend/internal/server/handler/response/recipe.go
@@ -1,8 +1,6 @@
 package response
 
 import (
-	"net/url"
-
 	"github.com/seka/reci-pin/backend/internal/domain/model"
 )
 
@@ -22,10 +20,9 @@ type TagResponse struct {
 }
 
 type RecipeImageResponse struct {
-	ID        int64  `json:"id"`
-	RecipeID  int64  `json:"recipe_id"`
-	ImagePath string `json:"image_path"`
-	ImageURL  string `json:"image_url"`
+	ID       int64  `json:"id"`
+	RecipeID int64  `json:"recipe_id"`
+	ImageURL string `json:"image_url"`
 }
 
 type CreateRecipeImageResponse struct {
@@ -33,21 +30,15 @@ type CreateRecipeImageResponse struct {
 	UploadURL string              `json:"upload_url"`
 }
 
-func NewRecipeImage(i *model.RecipeImage, publicBaseURL *url.URL) RecipeImageResponse {
-	imageURL := ""
-	if publicBaseURL != nil && i.ImagePath != "" {
-		imageURL = publicBaseURL.JoinPath(i.ImagePath).String()
-	}
-
+func NewRecipeImageResponse(img model.PublicRecipeImage) RecipeImageResponse {
 	return RecipeImageResponse{
-		ID:        i.ID,
-		RecipeID:  i.RecipeID,
-		ImagePath: i.ImagePath,
-		ImageURL:  imageURL,
+		ID:       img.ID,
+		RecipeID: img.RecipeID,
+		ImageURL: img.ImageURL.String(),
 	}
 }
 
-func NewRecipe(m *model.Recipe, publicBaseURL *url.URL) *RecipeResponse {
+func NewRecipe(m *model.Recipe) *RecipeResponse {
 	tags := make([]TagResponse, 0, len(m.Tags))
 	for _, t := range m.Tags {
 		tags = append(tags, TagResponse{
@@ -58,7 +49,7 @@ func NewRecipe(m *model.Recipe, publicBaseURL *url.URL) *RecipeResponse {
 
 	images := make([]RecipeImageResponse, 0, len(m.Images))
 	for _, i := range m.Images {
-		images = append(images, NewRecipeImage(&i, publicBaseURL))
+		images = append(images, NewRecipeImageResponse(i))
 	}
 
 	return &RecipeResponse{
@@ -72,10 +63,38 @@ func NewRecipe(m *model.Recipe, publicBaseURL *url.URL) *RecipeResponse {
 	}
 }
 
-func NewRecipes(recipes []model.Recipe, publicBaseURL *url.URL) []RecipeResponse {
+func NewRecipes(recipes []model.Recipe) []RecipeResponse {
 	responses := make([]RecipeResponse, 0, len(recipes))
 	for _, r := range recipes {
-		responses = append(responses, *NewRecipe(&r, publicBaseURL))
+		responses = append(responses, *NewRecipe(&r))
 	}
 	return responses
+}
+
+func NewRecipeResponse(recipe *model.Recipe) *RecipeResponse {
+	// This function seems to be a duplicate of NewRecipe, but the instruction explicitly asks for it.
+	// The image handling here is also slightly different (slice of pointers vs slice of values).
+	// I will implement it as provided in the snippet, assuming `recipe.Images` contains `model.PublicRecipeImage`.
+	images := make([]RecipeImageResponse, len(recipe.Images))
+	for i := range recipe.Images {
+		images[i] = NewRecipeImageResponse(recipe.Images[i])
+	}
+
+	tags := make([]TagResponse, 0, len(recipe.Tags))
+	for _, t := range recipe.Tags {
+		tags = append(tags, TagResponse{
+			ID:   t.ID,
+			Name: t.Name,
+		})
+	}
+
+	return &RecipeResponse{
+		ID:     recipe.ID,
+		UserID: recipe.UserID,
+		Name:   recipe.Name,
+		URL:    recipe.URL,
+		Memo:   recipe.Memo,
+		Tags:   tags,
+		Images: images,
+	}
 }
